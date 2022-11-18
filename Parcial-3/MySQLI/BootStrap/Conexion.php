3584d0abeb45790113f2e1489e7939bc6f8ddfdc
OrtegaJose19100229 <79618472+OrtegaJose19100229@users.noreply.github.com>
2022-11-18 13:45:34 -0600

    MYSQLI

---
+++ Parcial-3/MySQLI/BootStrap/Conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+$servidor= "localhost";
+$basedatos = "catalogojuegos";
+$usuario = "root";
+$password ="19100229Jo-0";
+
+$con = mysqli_connect($servidor, $usuario, $password, $basedatos) or die ("No se pudo conectar");
+$consulta = "select * from juego";
+
+$registros = mysqli_query($con, $consulta)or die ("Problemas en el select");
+// $registros = mysqli_query($con,$consulta) or die ("Problemas con el select");
+// while($reg = mysqli_fetch_array($registros,MYSQLI_NUM)){
+//     printf($reg['nombre'].' ')
+// }
+
+$result = mysqli_fetch_all($registros);
+
+mysqli_close($con);
+echo json_encode($result);
+?>
