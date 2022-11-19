eb083336ac70982555ea7de8cdb912ff23a4d3d0
OrtegaJose19100229 <79618472+OrtegaJose19100229@users.noreply.github.com>
2022-11-19 01:46:15 -0600

    xd

--- Parcial-3/MySQLI/BootStrap/Conexion.php
+++ Parcial-3/MySQLI/BootStrap/Conexion.php
@@ -1,12 +1,13 @@
 <?php
-
+$id= $_POST['Ano_Lanzamiento'];
 $servidor= "localhost";
 $basedatos = "catalogojuegos";
 $usuario = "root";
 $password ="19100229Jo-0";
+$ID=2;
 
 $con = mysqli_connect($servidor, $usuario, $password, $basedatos) or die ("No se pudo conectar");
-$consulta = "select * from juego";
+$consulta = "select * from juego where ID=$ID";
 
 $registros = mysqli_query($con, $consulta)or die ("Problemas en el select");
 // $registros = mysqli_query($con,$consulta) or die ("Problemas con el select");
