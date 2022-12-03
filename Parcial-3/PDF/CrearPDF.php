5bab0ffdd1ed5e1dcd9ddc4f2290cc8ee958f802
OrtegaJose19100229 <79618472+OrtegaJose19100229@users.noreply.github.com>
2022-12-02 22:01:27 -0600

    PDF

---
+++ Parcial-3/PDF/CrearPDF.php
@@ -0,0 +1,10 @@
+<?php
+$nombre=$_POST['nombre'];
+require('./FPDF/fpdf.php');
+$pdf = new FPDF();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(40,10,'¡Hola, Mundo!'.$nombre);
+$pdf->Output();
+?>
+
