bb60227e14f2fc44a561fce713f5e6509d72eca5
JOSEORTEGA19 <ortegajose072@gmail.com>
2023-09-17 17:04:13 -0500

    xd

--- TABD/books.js
+++ TABD/books.js
@@ -1,38 +1,32 @@
-document.addEventListener("DOMContentLoaded", function () {
-    // Obtener el contenedor
-    const container = document.querySelector(".container");
-
-    // Parsear el XML
-    const parser = new DOMParser();
-    const xmlDoc = parser.parseFromString(books, "books.xml"); // Reemplaza xmlString con tu XML completo
-
-    // Obtener la lista de libros
-    const books = xmlDoc.querySelectorAll("book");
-
-    // Recorrer la lista de libros y generar el contenido
-    books.forEach(book => {
-        const title = book.querySelector("title").textContent;
-        const author = book.querySelector("author").textContent;
-        const genre = book.querySelector("genre").textContent;
-        const price = book.querySelector("price").textContent;
-        const publishDate = book.querySelector("publish_date").textContent;
-        const description = book.querySelector("description").textContent;
-
-        // Crear un elemento para el libro
-        const bookElement = document.createElement("div");
-        bookElement.classList.add("book");
-
-        // Crear contenido para el libro
-        bookElement.innerHTML = `
-            <div class="title">${title}</div>
-            <div class="author">${author}</div>
-            <div class="genre">Genre: ${genre}</div>
-            <div class="price">Price: $${price}</div>
-            <div class="publish_date">Publish Date: ${publishDate}</div>
-            <div class="description">${description}</div>
-        `;
-
-        // Agregar el libro al contenedor
-        container.appendChild(bookElement);
-    });
+document.addEventListener("DOMContentLoaded", function() {
+    // Cargar el documento XML
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            mostrarXML(this.responseXML);
+        }
+    };
+    xhttp.open("GET", "books.xml", true);
+    xhttp.send();
 });
+
+function mostrarXML(xml) {
+    var contenedor = document.getElementById("contenedor");
+
+    // Obtener todos los elementos <elemento> del XML
+    var elementos = xml.getElementsByTagName("elemento");
+
+    // Recorrer los elementos y mostrarlos en cajas
+    for (var i = 0; i < elementos.length; i++) {
+        var elemento = elementos[i];
+        var contenido = elemento.textContent;
+
+        // Crear una nueva caja para cada elemento
+        var caja = document.createElement("div");
+        caja.className = "caja";
+        caja.textContent = contenido;
+
+        // Agregar la caja al contenedor
+        contenedor.appendChild(caja);
+    }
+}
